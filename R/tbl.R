76597c518fcd37798f6489b022ffdc09ad33c49c
Felipe Mattioni Maturana <felipemattioni92@gmail.com>
2021-01-27 19:17:14 +0100

    added metadata

--- R/tbl.R
+++ R/tbl.R
@@ -19,7 +19,18 @@ new_whippr_tibble <- function(.data, metadata) {
     time_column = metadata$time_column,
     vo2_column = metadata$vo2_column,
     test_type = metadata$test_type,
-    processed_data = metadata$processed_data
+    processed_data = metadata$processed_data,
+    incremental = metadata$incremental,
+    normalized = metadata$normalized,
+    incremental_type = metadata$incremental_type,
+    has_baseline = metadata$has_baseline,
+    baseline_length = metadata$baseline_length,
+    baseline_intensity = metadata$baseline_intensity,
+    ramp_increase = metadata$ramp_increase,
+    step_start = metadata$step_start,
+    step_increase = metadata$step_increase,
+    step_length = metadata$step_length,
+    outliers_detected = metadata$outliers_detected
   )
 
   tibble::validate_tibble(whippr_tibble)
@@ -47,6 +58,80 @@ print.whippr <- function(x, ...) {
 
   if(!is.null(attr(x, "test_type")))
     header <- paste0(header, "# Test type: ", attr(x, "test_type"), "\n")
+
+  # INCREMENTAL -------------------------------------------------------------
+
+  if(!is.null(attr(x, "incremental"))) {
+    if(attr(x, "incremental_type") == "ramp") {
+      if(!is.null(attr(x, "has_baseline"))) {
+        header <- paste0(header, "# Protocol: ",
+                         attr(x, "baseline_length"),
+                         "-s baseline at ",
+                         attr(x, "baseline_intensity"),
+                         " W and a ",
+                         attr(x, "ramp_increase"),
+                         "-W/min ramp",
+                         "\n")
+      } else {
+        header <- paste0(header, "# Protocol: ",
+                         attr(x, "no baseline"),
+                         " and a ",
+                         attr(x, "ramp_increase"),
+                         "-W/min ramp",
+                         "\n")
+      }
+    } else if(attr(x, "incremental_type") == "step") {
+      if(!is.null(attr(x, "has_baseline"))) {
+        if(!is.null(attr(x, "step_start"))) {
+          header <- paste0(header, "# Protocol: ",
+                           attr(x, "baseline_length"),
+                           "-s baseline at ",
+                           attr(x, "baseline_intensity"),
+                           " W and ",
+                           attr(x, "step_length"),
+                           "-s steps of ",
+                           attr(x, "step_increase"),
+                           " W starting at ",
+                           attr(x, "step_start"),
+                           " W",
+                           "\n")
+        } else {
+          header <- paste0(header, "# Protocol: ",
+                           attr(x, "baseline_length"),
+                           "-s baseline at ",
+                           attr(x, "baseline_intensity"),
+                           " W and ",
+                           attr(x, "step_length"),
+                           "-s steps of ",
+                           attr(x, "step_increase"),
+                           " W",
+                           "\n")
+        }
+      } else {
+        if(!is.null(attr(x, "step_start"))) {
+          header <- paste0(header, "# Protocol: ",
+                           attr(x, "no baseline"),
+                           " and ",
+                           attr(x, "step_length"),
+                           "-s steps of ",
+                           attr(x, "step_increase"),
+                           " W starting at ",
+                           attr(x, "step_start"),
+                           " W",
+                           "\n")
+        } else {
+          header <- paste0(header, "# Protocol: ",
+                           attr(x, "no baseline"),
+                           " and ",
+                           attr(x, "step_length"),
+                           "-s steps of ",
+                           attr(x, "step_increase"),
+                           " W",
+                           "\n")
+        }
+      }
+    }
+  }
 
   cat(pillar::style_subtle(header))
 
